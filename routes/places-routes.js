0cfe6b98b9a57019486a4edb3650d48e107eafe7
Arend Estes <42525228+arendestes@users.noreply.github.com>
2020-07-14 14:08:13 -0500

    set up app.js/ created route files/ tested routes-places

---
+++ routes/places-routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    console.log('GET request in places');
+    res.json({messege: "it works"});
+});
+
+
+
+
+module.exports = router;
