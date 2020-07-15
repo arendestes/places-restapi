2591c1bb6cc5dab92cafffec8aa1f49be87708fa
Arend Estes <42525228+arendestes@users.noreply.github.com>
2020-07-15 08:52:30 -0500

    added rout for specific place

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,9 +2,37 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    console.log('GET request in places');
-    res.json({messege: "it works"});
+const USER_DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Billy Bobbs Gator Farm and Petting Zoo',
+        description: 'Great place to wrestle a gator, pet a gator, ride a gator... pretty much anything goes.',
+        creator: 'u1',
+        address: '100 Swamp Britches Ln',
+        location: {
+            lat: 30.4457497, 
+            lng: -91.1871759
+        },
+        image: 'https://images.unsplash.com/photo-1520542099817-0d19524eccca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    }, 
+    {
+        id: 'p2',
+        title: 'Ricky Roys Gator Farm and Petting Zoo',
+        description: 'Great place to wrestle a gator, pet a gator, ride a gator... pretty much anything goes.',
+        creator: 'u2',
+        address: '100 Swamp Britches Ln',
+        location: {
+            lat: 30.4457497, 
+            lng: -91.1871759
+        },
+        image: 'https://images.unsplash.com/photo-1520542099817-0d19524eccca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    }
+];
+
+router.get('/:pid', (req, res, next) => {
+    const returnID = req.params.pid;
+    const place = USER_DUMMY_PLACES.find(place => place.id === returnID);
+    res.json({place});
 });
 
 
