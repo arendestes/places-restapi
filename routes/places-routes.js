ca0edebc48961258db82a55d8fed146b67010b71
Arend Estes <42525228+arendestes@users.noreply.github.com>
2020-07-15 13:41:41 -0500

    added controllers for places routes

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,65 +1,14 @@
 const express = require('express');
 
-const HttpError = require('../models/http-error');
+const placesControllers = require('../controllers/places-controllers')
 
 const router = express.Router();
 
-const USER_DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Billy Bobbs Gator Farm and Petting Zoo',
-        description: 'Great place to wrestle a gator, pet a gator, ride a gator... pretty much anything goes.',
-        creator: 'u1',
-        address: '100 Swamp Britches Ln',
-        location: {
-            lat: 30.4457497, 
-            lng: -91.1871759
-        },
-        image: 'https://images.unsplash.com/photo-1520542099817-0d19524eccca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
-    }, 
-    {
-        id: 'p3',
-        title: 'Billy Bobbs Gator Farm and mini golf',
-        description: 'Great place to wrestle a gator, pet a gator, ride a gator... pretty much anything goes.',
-        creator: 'u1',
-        address: '100 Swamp Britches Ln',
-        location: {
-            lat: 30.4457497, 
-            lng: -91.1871759
-        },
-        image: 'https://images.unsplash.com/photo-1520542099817-0d19524eccca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
-    },
-    {
-        id: 'p2',
-        title: 'Ricky Roys Gator Farm and Petting Zoo',
-        description: 'Great place to wrestle a gator, pet a gator, ride a gator... pretty much anything goes.',
-        creator: 'u2',
-        address: '100 Swamp Britches Ln',
-        location: {
-            lat: 30.4457497, 
-            lng: -91.1871759
-        },
-        image: 'https://images.unsplash.com/photo-1520542099817-0d19524eccca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
-    }
-];
 
-router.get('/:pid', (req, res, next) => {
-    const returnID = req.params.pid;
-    const place = USER_DUMMY_PLACES.find(place => place.id === returnID);
-    if(!place){
-        return next(new HttpError('Could not find place with pid.', 404));
-    };
-    res.json({place});
-});
 
-router.get('/user/:uid', (req, res, next) => {
-    const returnID = req.params.uid;
-    const places = USER_DUMMY_PLACES.filter(place => place.creator === returnID);
-    if(!places[0]){
-        return next(new HttpError('Could not find places with uid.', 404));
-    }
-    res.json({places});
-});
+router.get('/:pid', placesControllers.getPlaceBypid);
+
+router.get('/user/:uid', placesControllers.getPlacesByuid);
 
 
 module.exports = router;
